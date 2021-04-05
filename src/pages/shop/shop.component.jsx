60d443a6315894b75949969b2a23efd91aa61411
Nay Lwin <lwinnay54321@gmail.com>
2021-04-04 20:08:14 -0700

    implement adding cart features to cart reducers and grouping functionality to quantify same item with count

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { SHOP_DATA } from "./shop.data";
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import React from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
   state = {
@@ -8,14 +8,14 @@ class ShopPage extends React.Component {
   };
 
   render() {
-    const {collections} = this.state;
+    const { collections } = this.state;
     return (
       <div className="shop-page">
-        { collections.map(({id, ...otherCollectionProps}) => (
-           <CollectionPreview key={id} {...otherCollectionProps}/>
-         )) }
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
       </div>
-    )
+    );
   }
 }
 
