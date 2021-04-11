8ff6936368953454579075205787cf8b3ac7fb7a
Nay Lwin <lwinnay54321@gmail.com>
2021-04-10 20:10:46 -0700

    implement shop data pull from firebase to our shop page and also created HOC for loading page

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyD1Hk9LhgmT8neG5vePsr-i51l8fEhthdU',
-  authDomain: 'crwn-db-a4dbe.firebaseapp.com',
-  projectId: 'crwn-db-a4dbe',
-  storageBucket: 'crwn-db-a4dbe.appspot.com',
-  messagingSenderId: '879695779564',
-  appId: '1:879695779564:web:ceab453b4e875cd26c77c9',
+  apiKey: "AIzaSyD1Hk9LhgmT8neG5vePsr-i51l8fEhthdU",
+  authDomain: "crwn-db-a4dbe.firebaseapp.com",
+  projectId: "crwn-db-a4dbe",
+  storageBucket: "crwn-db-a4dbe.appspot.com",
+  messagingSenderId: "879695779564",
+  appId: "1:879695779564:web:ceab453b4e875cd26c77c9",
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
@@ -28,7 +28,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
   return userRef;
@@ -36,11 +36,42 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 firebase.initializeApp(config);
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    // generate new keys for each doc and return ref
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
